37dd50c34aebd5122c12a778b103c5550867e66a
kapana <giorgikapanadze9169@gmail.com>
2025-07-04 23:52:35 +0400

    vbuqsaob magrad

--- otxSaatiani_tutorial/Program.cs
+++ otxSaatiani_tutorial/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace otxSaatiani_tutorial
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("bidzinas dedas movtynav\n " +
+                "vnaxot aba ra moxdeba");
+
+            /*
+            string folder = @"C:\Users\Gio\Desktop\Programireba\Cshar_tavidan\otxSaatiani_tutorial";
+            // Filename
+            string fileName = "sikvdilisSia.txt";
+            // Fullpath. You can direct hardcode it if you like.
+            string fullPath = fileName;
+            // An array of strings
+            string[] authors = {"yornis dzvali"};
+            // Write array of strings to a file using WriteAllLines.
+            // If the file does not exists, it will create a new file.
+            // This method automatically opens the file, writes to it, and closes file
+            File.WriteAllLines(fullPath, authors);
+            // Read a file
+            string readText = File.ReadAllText(fullPath);
+            Console.WriteLine(readText);
+            */
         }
     }
 }
