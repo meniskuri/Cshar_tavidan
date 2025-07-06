0f5155f18cece0071c187fde8753c59f1433fee6
kapana <giorgikapanadze9169@gmail.com>
2025-07-06 22:19:00 +0400

    shecdoma vipove gavaswore - dasamtavrebeli damrcha programa

--- otxSaatiani_tutorial/Program.cs
+++ otxSaatiani_tutorial/Program.cs
@@ -33,6 +33,7 @@ namespace otxSaatiani_tutorial
 
             string fileName        = "";        // მომხმარებლის მიერ შექმნილი ფაილის სახელი (.txt ს გარეშე)
             string filePath        = "";        //
+            
             string text            = "";        // შეყვანილი ტექსტი
             string readText        = "";        // ტექსტის ამოკითხვა
 
@@ -40,13 +41,23 @@ namespace otxSaatiani_tutorial
             fullPath_jado   = Path.Combine(Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..", "..")), folderName_jado);
             // aq unda iyos faili statistikis  
 
-            // მომხმარებლის მიერ ფოლდერის შეყვანა და ფოლდერის სრული მისამართი
+            // მომხმარებლის მიერ ფოლდერის შეყვანა და ფოლდერის სრული მისამართი ფოლდერის შექმნა (თუ არ არსებობს)
             Console.Write("enter folder name: ");
             folderName = Console.ReadLine();
             Console.WriteLine("folder name is " + folderName);
 
             fullPath = Path.Combine(Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "..", "..")), fullPath_jado, folderName);
-            Console.WriteLine("fullpath_folder is " + fullPath);
+            Console.WriteLine("fullpath is " + fullPath);
+
+            if (!Directory.Exists(fullPath))
+            {
+                Directory.CreateDirectory(fullPath);
+                Console.WriteLine("folder created: " + fullPath);
+            }
+            else
+            {
+                Console.WriteLine("folder exist: " + fullPath);
+            }
 
             // მომხმარებლის მიერ ფაილის სახელის შეყვანა და ფაილის სრული მისამართი
             Console.Write("enter file name (without .txt): ");
@@ -61,29 +72,19 @@ namespace otxSaatiani_tutorial
             // როგორ ავირჩიო ორით უკან ფაილი? 
             // kai jer chavwero faili shignit da mere vnaxot aba ra da rogor gadavaketo
 
-            // ფოლდერის შექმნა (თუ არ არსებობს)
-            if (!Directory.Exists(fullPath))
-            {
-                Directory.CreateDirectory(fullPath);
-                Console.WriteLine("folder created: " + fullPath);
-            } else
-            {
-                Console.WriteLine("folder exist: " + fullPath);
-            }
-
             // ტექსტის შეყვანა
             Console.Write("enter text for file: ");
             text = Console.ReadLine();
+
+            // ტექსტის ჩაწერა ფაილში 
+            // File.WriteAllText(filePath, text);
+            File.AppendAllText(filePath, text + Environment.NewLine); ;
+            Console.WriteLine("text will save in file: " + filePath);
 
             // ლაინების რაოდენობას ითვლის ფოლდერში
             lineCount = File.ReadAllLines(filePath).Length;
             Console.WriteLine("lineCount in file is " + lineCount);
 
-            // ტექსტის ჩაწერა ფაილში
-            // File.WriteAllText(filePath, text);
-            File.AppendAllText(filePath, text + Environment.NewLine); ;
-            Console.WriteLine("text will save in file: " + filePath);
-            
 
             // ფაილიდან წაკითხვა
             readText = File.ReadAllText(filePath);
