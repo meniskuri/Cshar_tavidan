67605f4e342d76962e61891d0494d804289ae5dd
kapana <giorgikapanadze9169@gmail.com>
2025-07-12 19:01:24 +0400

    shevqmeni klasi calke da main methodshi shevqmeni obieqti da gamovizaxe

---
+++ tavi_3_klasebi/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tavi_3_klasebi
+{
+    internal class Person
+    {
+        //
+        public string name = "Undefined";   // имя
+        public int age;                     // возраст
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+}
---
+++ tavi_3_klasebi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tavi_3_klasebi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // shevqmant konstruqtorit obieqti
+            Person giorgi = new Person();
+
+            Console.WriteLine(giorgi);
+            giorgi.name = "giorgi";
+            giorgi.age = 33;
+            giorgi.Print();
+            
+        }
+    }
+}
