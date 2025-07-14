9adb3e0ecc4bb21fc732c0ca8e439c4a45130762
kapana <giorgikapanadze9169@gmail.com>
2025-07-14 22:02:14 +0400

    konstruqtorebi davamate

---
+++ klasebi_mecadineoba/Person3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasebi_mecadineoba
+{
+    internal class Person3
+    {
+        public string name;
+        public int age;
+        /*
+        public Person3() : this("Неизвестно")    // первый конструктор
+        { }
+        public Person3(string name) : this(name, 18) // второй конструктор
+        { }
+        */
+        public Person3(string name , int age)     // третий конструктор
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public void Print() => Console.WriteLine($"Имя: {name}  Возраст: {age}");
+    }
+}
--- klasebi_mecadineoba/Program.cs
+++ klasebi_mecadineoba/Program.cs
@@ -5,12 +5,46 @@ using klasebi_mecadineoba;
 Console.WriteLine("Hello, jadoqar anu kapan!");
 
 // obieqtebi
-Person gio = new Person();
+Person gio = new ();
 gio.name = "aruza";
 gio.Print();
 
-Person2 tom_ridli = new Person2();
+Person2 tom_ridli = new ();
 tom_ridli.name = "me var tom ridli";
 tom_ridli.Print();
 
+Person3 hari = new("giorgi",10);
+hari.Print();
+
+// struct - structurebi ravi vnaxot aba
+Console.WriteLine("//////////////");
+Adamiani tom = new ();
+tom.Print();    // Имя:Tom  Возраст: 1
+
+Console.WriteLine("//////////////");
+Adamiani2 tamri = new("tamri",57);
+tamri.Print();
+
+struct Adamiani
+{
+    // инициализация полей значениями по умолчанию - доступна с C#10
+    public string name;
+    public int age;
+    public Adamiani() { }
+    public void Print() => Console.WriteLine($"Имя: {name}  Возраст: {age}");
+}
+
+struct Adamiani2
+{
+    // struqtura konstruqtorit
+    public string name;
+    public int age;
+
+    public Adamiani2(string name , int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void Print() => Console.WriteLine($"Имя: {name}  Возраст: {age}");
+}
 
