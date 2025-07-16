9c5d7b0cab75e4704ea97d1c10d7093cef0ea57a
kapana <giorgikapanadze9169@gmail.com>
2025-07-16 20:40:51 +0400

    tvisebebze var - ravi ragac gavige get da set - rashi gamoviyeno jer ver vxdebi amat

---
+++ tvisebebi_get_set_3_13/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Person person = new Person { Name = "gio" };
+
+// Устанавливаем свойство - срабатывает блок Set
+// значение "Tom" и есть передаваемое в свойство value
+// person.Name = "Tom";
+
+// Получаем значение свойства и присваиваем его переменной - срабатывает блок Get
+string personName = person.Name;
+Console.WriteLine(personName);  // Tom
+
+class Person
+{
+    private string name = "Undefined";
+
+    public required string Name
+    {
+        get
+        {
+            return name + " yleo ";    // возвращаем значение свойства
+        }
+        set
+        {
+            name = value;   // устанавливаем новое значение свойства
+        }
+    }
+}
