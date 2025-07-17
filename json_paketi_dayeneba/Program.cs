db9b14426f4cc6ec9676ec98aab78ae1e656d75f
kapana <giorgikapanadze9169@gmail.com>
2025-07-17 17:55:39 +0400

    nuget sit davayene json paketi raylib ver dayenda

---
+++ json_paketi_dayeneba/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+ 
+Person tom = new Person("Tom", 38);
+// сериализуем объект tom в код json
+string json = JsonConvert.SerializeObject(tom);
+Console.WriteLine(json);
+ 
+ 
+class Person
+{
+    public string Name {get;set;}
+    public int Age {get;set;}
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
