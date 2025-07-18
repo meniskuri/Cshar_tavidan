1447a92a1700f11262d7109bb651658b0b887732
kapana <giorgikapanadze9169@gmail.com>
2025-07-18 18:20:51 +0400

    4 tavs vasruleb - generic inheritance ze var - bevri ragac gavige magram praqtika mchirdeba ese prosta vigeb da vso sheizleba damaviwydes

--- opp_inheritance_4_1/Program.cs
+++ opp_inheritance_4_1/Program.cs
@@ -4,6 +4,16 @@ bob.Print(); // вызов метода Print из класса Person
 Employee tom = new Employee("Tom", "Microsoft");
 tom.Print(); // вызов метода Print из класса Person
 
+
+Adamiani<string> adamiani1 = new Adamiani<string>("34");
+Adamiani<int> adamiani2 = new UniversalPerson<int>(45);
+UniversalPerson<int> adamiani3 = new UniversalPerson<int>(33);
+Console.WriteLine(adamiani1.Id);
+Console.WriteLine(adamiani2.Id);
+Console.WriteLine(adamiani3.Id);
+
+
+
 // კლასები
 class Person
 {
@@ -29,3 +39,19 @@ class Employee : Person
         Console.WriteLine($"{Name} работает в {Company}");
     }
 }
+
+// generic კლასები და მათი შთამომავლობა 
+class Adamiani<T>
+{
+    public T Id { get; }
+    public Adamiani(T id)
+    {
+        Id = id;
+    }
+}
+
+class UniversalPerson<T> : Adamiani<T>
+{
+    public UniversalPerson(T id) : base(id) { }
+}
+
