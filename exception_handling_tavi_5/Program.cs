3948071cb541b521e9ff6dccc6ca4e120321bdc8
kapana <giorgikapanadze9169@gmail.com>
2025-07-18 19:54:43 +0400

    exception ebis gamoyenebis sxvadasxva magaliti

---
+++ exception_handling_tavi_5/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+try
+{
+    string text = File.ReadAllText("data.txt");
+    Console.WriteLine(text);
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"faili ver moizebna {ex.StackTrace}");
+}
+
+// throw 
+try
+{
+    Console.Write("Введите имя: ");
+    string? name = Console.ReadLine();
+    if (name == null || name.Length < 2)
+    {
+        throw new Exception("Длина имени меньше 2 символов");
+    }
+    else
+    {
+        Console.WriteLine($"Ваше имя: {name}");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Ошибка: {e.Message}");
+}
