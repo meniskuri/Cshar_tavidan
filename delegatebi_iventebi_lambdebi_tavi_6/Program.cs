d01d459c6c0f35c2edc55db8df2cc8a36a55c0c9
kapana <giorgikapanadze9169@gmail.com>
2025-07-21 17:28:06 +0400

    interfeisebze gadavedi 7 tavi - 7.1 interfeisebis gansazgvra

--- delegatebi_iventebi_lambdebi_tavi_6/Program.cs
+++ delegatebi_iventebi_lambdebi_tavi_6/Program.cs
@@ -1,89 +1,36 @@
-﻿Message message1 = Welcome.Print;
-message1 += new Hello().Display;
-//Message message2 = new Hello().Display;
-
-message1(); // Welcome
-
-// ლამბდები
-Message2 hello = () => Console.WriteLine("Hello");
-hello();       // Hello
-hello();       // Hello
-hello();       // Hello
+﻿//
+Account account = new Account(200);
+account.RegisterHandler(PrintSimpleMessage);
+account.Take(100);
 
 
-// ივენთები გამოყენება 
-Account acc = new Account(100);
-acc.Notify += DisplayMessage; // ეს გავარჩიოთ 
-acc.Put(20);
-acc.Take(70);
-acc.Take(150);
 
-void DisplayMessage(Account sender, AccountEventArgs e)
+void PrintSimpleMessage(string message) => Console.WriteLine(message);
+// Объявляем делегат
+public delegate void AccountHandler(string message);
+public class Account
 {
-    Console.WriteLine($"Сумма транзакции: {e.Sum}");
-    Console.WriteLine(e.Message);
-    Console.WriteLine($"Текущая сумма на счете: {sender.Sum}");
-}
-
-
-// დელეგატები 
-
-delegate void Message();
-delegate void Message2();
-
-// ივენთებზე 
-class AccountEventArgs
-{
-    // Сообщение
-    public string Message { get; }
-    // Сумма, на которую изменился счет
-    public int Sum { get; }
-    public AccountEventArgs(string message, int sum)
+    int sum;
+    // Создаем переменную делегата
+    AccountHandler? taken;
+    public Account(int sum) => this.sum = sum;
+    // Регистрируем делегат
+    public void RegisterHandler(AccountHandler del)
     {
-        Message = message;
-        Sum = sum;
+        taken = del;
     }
-}
-
-class Account
-{
-    public delegate void AccountHandler(Account sender, AccountEventArgs e);
-    public event AccountHandler? Notify;
-
-    public int Sum { get; private set; }
-
-    public Account(int sum) => Sum = sum;
-
-    public void Put(int sum)
-    {
-        Sum += sum;
-        Notify?.Invoke(this, new AccountEventArgs($"На счет поступило {sum}", sum));
-    }
+    public void Add(int sum) => this.sum += sum;
     public void Take(int sum)
     {
-        if (Sum >= sum)
+        if (this.sum >= sum)
         {
-            Sum -= sum;
-            Notify?.Invoke(this, new AccountEventArgs($"Сумма {sum} снята со счета", sum));
+            this.sum -= sum;
+            // вызываем делегат, передавая ему сообщение
+            taken?.Invoke($"Со счета списано {sum} у.е.");
         }
         else
         {
-            Notify?.Invoke(this, new AccountEventArgs("Недостаточно денег на счете", sum));
+            taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
         }
     }
-}
-
-
-
-
-
-
-
-class Welcome
-{
-     public static void Print() => Console.WriteLine("Welcome");
-}
-class Hello
-{
-    public void Display() => Console.WriteLine("Привет");
 }
---
+++ interfeisebi_tavi_7/Program.cs
@@ -0,0 +1,30 @@
+﻿// shevqmna obieqti
+IMovable modzraoba = new Modzraoba();
+modzraoba.Move();
+
+// interface shevqmeni
+interface IMovable
+{
+    // константа
+    const int minSpeed = 0;     // минимальная скорость
+    // статическая переменная
+    // static int maxSpeed = 60;   // максимальная скорость
+    // метод
+    void Move();                // движение
+    // свойство
+    // string Name { get; set; }   // название
+
+    delegate void MoveHandler(string message);  // определение делегата для события
+    // событие
+    // event MoveHandler MoveEvent;    // событие движения
+}
+
+// shevqmeni klasi
+class Modzraoba : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("imozrave");
+    }
+}
+
