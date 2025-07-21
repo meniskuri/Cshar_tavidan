1b2a20ce0af0f62439cf06c50a6835fc7550d2c6
kapana <giorgikapanadze9169@gmail.com>
2025-07-21 22:03:09 +0400

    clonireba gavakete obieqtebis - vmecadineob interfeisebs vamtavreb - xval gavagrzeleb

--- interfeisebi_tavi_7/Program.cs
+++ interfeisebi_tavi_7/Program.cs
@@ -1,8 +1,50 @@
 ﻿// shevqmna obieqti
+using System;
+using System.Xml.Linq;
+
 IMovable modzraoba = new Modzraoba();
+modzraoba.Name = "giorgi";
 modzraoba.Move();
 
+// გენერიკებით
+// მეთოდი ორი მნიშვნელობის შედარებისთვის
+T GetMax<T>(T x, T y) where T : IComparable<T> // kitxva 
+{
+    if (x.CompareTo(y) > 0)
+        return x;
+    else
+        return y;
+}
+
+int a = 5;
+int b = 8;
+int max = GetMax(a, b);
+Console.WriteLine(max); // 8
+
+
+var tom = new Person("Tom", 23);
+var bob = (Person)tom.Clone();
+bob.Name = "Bob";
+Console.WriteLine(tom.Name); // Tom
+
+
+
 // interface shevqmeni
+class Person : ICloneable // kitxva maqvs 
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public object Clone()
+    {
+        return new Person(Name, Age);
+    }
+}
+
 interface IMovable
 {
     // константа
@@ -10,9 +52,10 @@ interface IMovable
     // статическая переменная
     // static int maxSpeed = 60;   // максимальная скорость
     // метод
-    void Move();                // движение
+    void Move(); // движение
+  
     // свойство
-    // string Name { get; set; }   // название
+    string Name { get; set; }   // название
 
     delegate void MoveHandler(string message);  // определение делегата для события
     // событие
@@ -24,7 +67,9 @@ class Modzraoba : IMovable
 {
     public void Move()
     {
-        Console.WriteLine("imozrave");
+        Console.WriteLine($"imozrave {Name}");
     }
+    public string Name { get; set; }
 }
+
 
