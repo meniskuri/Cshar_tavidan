158a2766740cf51fde878828d241f0e0ef659ac5
kapana <giorgikapanadze9169@gmail.com>
2025-07-23 19:35:54 +0400

    tuples rogroc funqciidan dabrunebuli parametrebi

--- OPP_damatebiti_shesadzleblobebi_tavi_8/Program.cs
+++ OPP_damatebiti_shesadzleblobebi_tavi_8/Program.cs
@@ -56,7 +56,19 @@ Console.WriteLine("/// tuples ///");
 (int, string, bool) data = (25, "hello", true);
 Console.WriteLine(data.Item1);
 
+var tuple = GetValuesData(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+Console.WriteLine(tuple.count);
+Console.WriteLine(tuple.sum);
 
+(int sum, int count) GetValuesData(int[] numbers)
+{
+    var result = (sum: 0, count: numbers.Length);
+    foreach (var n in numbers)
+    {
+        result.sum += n;
+    }
+    return result;
+}
 
 
 // klasi veqtorebze
