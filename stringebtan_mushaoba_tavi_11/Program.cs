7f5001e8ed0058d740e3b98d9c47686f5b245fca
kapana <giorgikapanadze9169@gmail.com>
2025-07-25 20:12:07 +0400

    regex - nu es tavi davamtavre - davigale

--- stringebtan_mushaoba_tavi_11/Program.cs
+++ stringebtan_mushaoba_tavi_11/Program.cs
@@ -1,4 +1,7 @@
 ﻿///////////////////////////////////////
+
+using System.Text.RegularExpressions;
+
 /// Строки и класс String
 
 string s1 = "hello";
@@ -55,4 +58,38 @@ for (int i = 0; i < files.Length; i++)
 {
     if (files[i].EndsWith(".exe"))
         Console.WriteLine(files[i]);
+}
+
+string text = "И поэтому все так   произошло";
+
+string[] words = text.Split(new char[] { ' ' });
+
+foreach (string s in words)
+{
+    Console.WriteLine(s);
+}
+
+///////////////////////////////////////
+/// Форматирование строк
+Console.WriteLine("Форматирование строк");
+
+string name = "Tom";
+int age = 23;
+string output = string.Format("Имя: {0}  Возраст: {1}", name, age);
+Console.WriteLine(output);
+
+
+///////////////////////////////////////
+/// regex
+string str = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
+Regex regex = new Regex(@"туп(\w*)");
+MatchCollection matches = regex.Matches(str);
+if (matches.Count > 0)
+{
+    foreach (Match match in matches)
+        Console.WriteLine(match.Value);
+}
+else
+{
+    Console.WriteLine("Совпадений не найдено");
 }
