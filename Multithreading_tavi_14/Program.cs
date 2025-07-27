be6d642f2dc16c955c5ece3ea05fa7eabede7296
kapana <giorgikapanadze9169@gmail.com>
2025-07-27 16:38:31 +0400

    tavi 14 - patokebi - anu ragac multitaskingis nairi

---
+++ Multithreading_tavi_14/Program.cs
@@ -0,0 +1,37 @@
+﻿/////
+using System.Threading;
+
+// получаем текущий поток
+Thread currentThread = Thread.CurrentThread;
+
+//получаем имя потока
+Console.WriteLine($"Имя потока: {currentThread.Name}");
+currentThread.Name = "Метод Main";
+Console.WriteLine($"Имя потока: {currentThread.Name}");
+
+Console.WriteLine($"Запущен ли поток: {currentThread.IsAlive}");
+Console.WriteLine($"Id потока: {currentThread.ManagedThreadId}");
+Console.WriteLine($"Приоритет потока: {currentThread.Priority}");
+Console.WriteLine($"Статус потока: {currentThread.ThreadState}");
+
+
+
+
+Thread thread = new Thread(PrintMessage);
+thread.Start();  // ← მეორე ნაკადი იწყებს
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Main thread");
+    Thread.Sleep(500);
+}
+
+
+static void PrintMessage()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Secondary thread");
+        Thread.Sleep(500);
+    }
+}
