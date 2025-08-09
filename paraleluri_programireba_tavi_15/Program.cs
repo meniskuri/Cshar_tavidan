7786c4394118de31208c834ef2229233073b325f
kapana <giorgikapanadze9169@gmail.com>
2025-08-09 17:25:00 +0400

    tpl it taskebis gashveba paraleluri programireba

--- paraleluri_programireba_tavi_15/Program.cs
+++ paraleluri_programireba_tavi_15/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+
+Task task1 = new Task(() =>
+{
+    Console.WriteLine($"Task{Task.CurrentId} Starts");
+    Thread.Sleep(1000);
+    Console.WriteLine($"Task{Task.CurrentId} Ends");
+});
+task1.Start(); //запускаем задачу
+
+// получаем информацию о задаче
+Console.WriteLine($"task1 Id: {task1.Id}");
+Console.WriteLine($"task1 is Completed: {task1.IsCompleted}");
+Console.WriteLine($"task1 Status: {task1.Status}");
+
+task1.Wait(); // ожидаем завершения задачи
