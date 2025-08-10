d59c9d91e2d4a9483b706b9592e352e1350e6094
kapana <giorgikapanadze9169@gmail.com>
2025-08-10 18:35:21 +0400

    Task.WhenAll and Task.WhenAny

--- tavi_16_asinqronuli_programireba/Program.cs
+++ tavi_16_asinqronuli_programireba/Program.cs
@@ -15,3 +15,34 @@ async Task PrintAsync()
     Console.WriteLine("Конец метода PrintAsync");
 }
 
+Console.WriteLine("///////////////");
+
+// определяем и запускаем задачи
+var task1 = PrintAsync2("Hello C#");
+var task2 = PrintAsync2("Hello World");
+var task3 = PrintAsync2("Hello METANIT.COM");
+
+// ожидаем завершения всех задач
+await Task.WhenAll(task1, task2, task3);
+
+async Task PrintAsync2(string message)
+{
+    await Task.Delay(2000);     // имитация продолжительной операции
+    Console.WriteLine(message);
+}
+
+Console.WriteLine("///////////////");
+
+// определяем и запускаем задачи
+var task11 = PrintAsync3("Hello C#");
+var task22 = PrintAsync3("Hello World");
+var task33 = PrintAsync3("Hello METANIT.COM");
+
+// ожидаем завершения хотя бы одной задачи
+await Task.WhenAny(task11, task22, task33);
+
+async Task PrintAsync3(string message)
+{
+    await Task.Delay(new Random().Next(1000, 2000));     // имитация продолжительной операции
+    Console.WriteLine(message);
+}
