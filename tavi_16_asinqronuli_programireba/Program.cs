70378086a26f376c5109400859557e14bd10a9bb
kapana <giorgikapanadze9169@gmail.com>
2025-08-10 17:35:48 +0400

    16.1 asinqronuli metodis magaliti - gadavdivar 16.2 ze

--- tavi_16_asinqronuli_programireba/Program.cs
+++ tavi_16_asinqronuli_programireba/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿await PrintAsync();   // вызов асинхронного метода
+Console.WriteLine("Некоторые действия в методе Main");
+
+void Print()
+{
+    Thread.Sleep(3000);     // имитация продолжительной работы
+    Console.WriteLine("Hello METANIT.COM");
+}
+
+// определение асинхронного метода
+async Task PrintAsync()
+{
+    Console.WriteLine("Начало метода PrintAsync"); // выполняется синхронно
+    await Task.Run(Print);                // выполняется асинхронно
+    Console.WriteLine("Конец метода PrintAsync");
+}
+
