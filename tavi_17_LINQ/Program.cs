fc2f343db374cd8c1baaa3bfcd7343a7b8319f37
kapana <giorgikapanadze9169@gmail.com>
2025-08-12 18:00:54 +0400

    LINQ s pirveli magaliti

--- tavi_17_LINQ/Program.cs
+++ tavi_17_LINQ/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+
+// создаем новый список для результатов
+var selectedPeople = from p in people // передаем каждый элемент из people в переменную p
+                     where p.ToUpper().StartsWith("T") //фильтрация по критерию
+                     orderby p  // упорядочиваем по возрастанию
+                     select p; // выбираем объект в создаваемую коллекцию
+
+foreach (string person in selectedPeople)
+    Console.WriteLine(person);
