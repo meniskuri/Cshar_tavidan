a826737e34a632de0adbd95828b78d2fff061dcf
kapana <giorgikapanadze9169@gmail.com>
2025-08-18 14:46:45 +0400

    xml is pirveli wakitxva

--- tavi_24_XML/Program.cs
+++ tavi_24_XML/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml;
+
+XmlDocument xDoc = new XmlDocument();
+// kitxva ????
+xDoc.Load("C:\\Users\\Gio\\Desktop\\Programireba\\Cshar_tavidan\\tavi_24_XML\\obj\\Debug\\net9.0\\people.xml");
+// получим корневой элемент
+XmlElement? xRoot = xDoc.DocumentElement;
+if (xRoot != null)
+{
+    // обход всех узлов в корневом элементе
+    foreach (XmlElement xnode in xRoot)
+    {
+        // получаем атрибут name
+        XmlNode? attr = xnode.Attributes.GetNamedItem("name");
+        Console.WriteLine(attr?.Value);
+
+        // обходим все дочерние узлы элемента user
+        foreach (XmlNode childnode in xnode.ChildNodes)
+        {
+            // если узел - company
+            if (childnode.Name == "company")
+            {
+                Console.WriteLine($"Company: {childnode.InnerText}");
+            }
+            // если узел age
+            if (childnode.Name == "age")
+            {
+                Console.WriteLine($"Age: {childnode.InnerText}");
+            }
+        }
+        Console.WriteLine();
+    }
+}
