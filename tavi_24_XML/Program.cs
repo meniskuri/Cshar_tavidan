c83018cfbe720726c73131cce56ff99c83630f0e
kapana <giorgikapanadze9169@gmail.com>
2025-08-23 19:29:51 +0400

    xml dokumentshi damateba elementebis

--- tavi_24_XML/Program.cs
+++ tavi_24_XML/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Xml;
 
+var people = new List<Person>();
+
 XmlDocument xDoc = new XmlDocument();
 // kitxva ????
 xDoc.Load("C:\\Users\\Gio\\Desktop\\Programireba\\Cshar_tavidan\\tavi_24_XML\\obj\\Debug\\net9.0\\people.xml");
@@ -7,27 +9,61 @@ xDoc.Load("C:\\Users\\Gio\\Desktop\\Programireba\\Cshar_tavidan\\tavi_24_XML\\ob
 XmlElement? xRoot = xDoc.DocumentElement;
 if (xRoot != null)
 {
-    // обход всех узлов в корневом элементе
     foreach (XmlElement xnode in xRoot)
     {
-        // получаем атрибут name
+        Person person = new Person();
         XmlNode? attr = xnode.Attributes.GetNamedItem("name");
-        Console.WriteLine(attr?.Value);
-
-        // обходим все дочерние узлы элемента user
+        person.Name = attr?.Value;
+ 
         foreach (XmlNode childnode in xnode.ChildNodes)
         {
-            // если узел - company
             if (childnode.Name == "company")
-            {
-                Console.WriteLine($"Company: {childnode.InnerText}");
-            }
-            // если узел age
+                person.Company = childnode.InnerText;
+ 
             if (childnode.Name == "age")
-            {
-                Console.WriteLine($"Age: {childnode.InnerText}");
-            }
+                person.Age = int.Parse(childnode.InnerText);
         }
-        Console.WriteLine();
+        people.Add(person);
     }
+    foreach (var person in people)
+        Console.WriteLine($"{person.Name} ({person.Company}) - {person.Age}");
+}
+
+
+// создаем новый элемент person
+XmlElement personElem = xDoc.CreateElement("person");
+
+// создаем атрибут name
+XmlAttribute nameAttr = xDoc.CreateAttribute("name");
+
+// создаем элементы company и age
+XmlElement companyElem = xDoc.CreateElement("company");
+XmlElement ageElem = xDoc.CreateElement("age");
+
+// создаем текстовые значения для элементов и атрибута
+XmlText nameText = xDoc.CreateTextNode("Mark");
+XmlText companyText = xDoc.CreateTextNode("Facebook");
+XmlText ageText = xDoc.CreateTextNode("30");
+
+//добавляем узлы
+nameAttr.AppendChild(nameText);
+companyElem.AppendChild(companyText);
+ageElem.AppendChild(ageText);
+
+// добавляем атрибут name
+personElem.Attributes.Append(nameAttr);
+// добавляем элементы company и age
+personElem.AppendChild(companyElem);
+personElem.AppendChild(ageElem);
+// добавляем в корневой элемент новый элемент person
+xRoot?.AppendChild(personElem);
+// сохраняем изменения xml-документа в файл
+xDoc.Save("people2.xml");
+
+
+class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public string? Company { get; set; }
 }
